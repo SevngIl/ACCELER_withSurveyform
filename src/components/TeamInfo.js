2dc0ff8fe0cd43caced0028369957be2b94bcfa2
SevngIl <96164112+SevngIl@users.noreply.github.com>
2023-03-16 22:08:35 +0900

    Fix Team Info  (#11)
    * Add Team Info
    
    * TeamInfo- flexible display and team name
    
    * fix: teaminfo
    
    * Delete unecessary code
    
    ---------
    
    Co-authored-by: chromato99 <chromato99@gmail.com>

--- src/components/TeamInfo.js
+++ src/components/TeamInfo.js
@@ -1,52 +1,43 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
 
 export default function TeamInfo() {
   return (
     <div id='d0'>
-      <div className="text_1">
-        <h1>Team members</h1>
+      <div class="text_1">
+      <div className='teaminfo_description' >
+      <h1>"Advanced Coding, Cloud Engineering, and Lifelong Execution with Reliability"</h1>
       </div>
+      <div className='team_members'> 
+        Team members
+      </div>
+      </div>
+
       <div id='d1'>
-        <Card className='mem_group'>
-          <Card.Body>
-            <img name="seungil" className="profile_img" src={require("../assets/profile/Seungil.png")} alt="profile" />
-            <p className="text_name"><b>Seungil Han</b></p>
-            <p>thesence01@gachon.ac.kr</p>
-          </Card.Body>
-        </Card>
-
-        <Card className='mem_group'>
-          <Card.Body>
-            <img name="jimin" className="profile_img" src={require("../assets/profile/Jimin.png")} alt="profile" />
-            <p className="text_name"><b>Jimin Lee</b></p>
-            <p>reakin@gachon.ac.kr</p>
-          </Card.Body>
-        </Card>
-
-        <Card className='mem_group'>
-          <Card.Body>
-            <img name="doyeon" className="profile_img" src={require("../assets/profile/Doyeon.png")} alt="profile" />
-            <p className="text_name"><b>Doyeon Hyun</b></p>
-            <p>118ssun@gachon.ac.kr</p>
-          </Card.Body>
-        </Card>
-
-        <Card className='mem_group'>
-          <Card.Body>
-            <img name="bumgi" className="profile_img" src={require("../assets/profile/Beomgi.png")} alt="profile" />
-            <p className="text_name"><b>Beomgi Kim</b></p>
-            <p>chromato99@gachon.ac.kr</p>
-          </Card.Body>
-        </Card>
-
-        <Card className='mem_group'>
-          <Card.Body>
-            <img name="yejin" className="profile_img" src={require("../assets/profile/Yejin.png")} alt="profile" />
-            <p className="text_name"><b>Yejin Choi</b></p>
-            <p>chldppwls12@gachon.ac.kr</p>
-          </Card.Body>
-        </Card>
+        <div class='mem_group'>
+          <img name="seungil" class = "profile_img" src= {require("../assets/profile/Seungil.png")} alt="profile"/>
+          <p class="text_name"><b>Seungil Han</b></p>
+          <p>thesence01@gachon.ac.kr</p>
+        </div>
+        <div class='mem_group'>
+          <img name="jimin" class = "profile_img" src= {require("../assets/profile/Jimin.png")} alt="profile"/>
+          <p class="text_name"><b>Jimin Lee</b></p>
+          <p>reakin@gachon.ac.kr</p>
+        </div>
+        <div class='mem_group'>
+        <img name="doyeon" class = "profile_img" src= {require("../assets/profile/Doyeon.png")} alt="profile"/>
+          <p class="text_name"><b>Doyeon Hyun</b></p>
+          <p>118ssun@gachon.ac.kr</p>
+        </div>
+        <div class='mem_group'>
+          <img name="bumgi" class = "profile_img" src= {require("../assets/profile/Beomgi.png")} alt="profile"/>
+          <p class="text_name"><b>Beomgi Kim</b></p>
+          <p>chromato99@gachon.ac.kr</p>
+        </div>
+        <div class='mem_group'> 
+          <img name="yejin" class = "profile_img" src= {require("../assets/profile/Yejin.png")} alt="profile"/>
+          <p class="text_name"><b>Yejin Choi</b></p>
+          <p>chldppwls12@gachon.ac.kr</p>
+        </div>
       </div>
     </div>
   )
